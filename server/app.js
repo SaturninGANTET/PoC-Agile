a9284e8c8730bec1408984de3306defa40ae5545
saturninGANTET <saturnin.gantet@hotmail.fr>
2021-01-31 13:11:44 +0000

    Web service REST -> reception et sauvegarde des images

--- server/app.js
+++ server/app.js
@@ -9,6 +9,31 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(morgan('short'));
 
+const storage = multer.diskStorage({
+        destination: function(req,file,cb){
+                cb(null,"public/puush/");
+        },
+        filename: function(req,file,cb){
+                cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        }
+});
+
+app.post('/api/puush/up',(req,res) => {
+     let upload = multer({ storage: storage}).single('image');
+     upload(req, res, function(err) {
+        if(req.fileValidationError) {
+                return res.send(req.fileValidationError);
+        } else if (!req.file) {
+                return res.send('Please select an image to upload');
+        } else if (err instanceof multer.MulterError){
+                return res.send(err);
+        } else if (err) {
+                return res.send(err);
+        }
+
+        res.send(`http://satutu.ddns.net:3004/puush/${req.file.filename}`);
+    });
+});
 
 app.listen(3004, () => {
     console.log(`Server is up and listening on port 3004...`);
